b8edfe54b4c2690b93dabaf4e85fb3b6c83e8e69
Foboz <147492184+fobozongithub@users.noreply.github.com>
2024-05-14 13:33:07 +0200

    Update fxmanifest.lua

--- fobozo-npcdialogue/fxmanifest.lua
+++ fobozo-npcdialogue/fxmanifest.lua
@@ -8,6 +8,6 @@ version "1.0.0"
 ui_page "ui/index.html"
 files { "src/ui/**/**" }
 
-shared_scripts { 'shared/*.lua', '@es_extended/imports.lua' }
+shared_scripts { 'shared/*.lua' }
 client_script 'src/client/*.lua'
 server_script 'src/server/*.lua'
