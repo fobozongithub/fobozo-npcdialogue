dfb499218d9d24a65c094809c3d0039a913d3d8a
Foboz <147492184+fobozongithub@users.noreply.github.com>
2024-05-15 11:33:26 +0200

    Update shared.lua

--- fobozo-npcdialogue/shared/shared.lua
+++ fobozo-npcdialogue/shared/shared.lua
@@ -109,3 +109,35 @@ Shared.DialoguePeds = {
         }
     },
 }
+
+-- // [HOW TO USE EXPORT] \\ --
+
+-- exports['fobozo-npcdialogue']:createDialoguePed(
+--     "csb_burgerdrug", -- // [Ped model] \\ --
+--     "Abu Salman", -- // [Ped name] \\ --
+--     "Burger Shot Employee", -- // [Job title] \\ --
+--     "police", -- // [Job requirement (optional, leave empty if none)] \\ --
+--     -1173.4746, -882.8961, 13.0092, 32.2758, -- // [Coordinates (x, y, z, w)] \\ --
+--     "Hello, do you want to start or end your shift?", -- // [Text] \\ --
+--     "ox_target", -- // [Interaction type ('ox_target' or 'interact')] \\ --
+--     { -- // [Options] \\ --
+--         {
+--             label = "Sign In/Out",
+--             event = "fobozo:print", 
+--             type = "client", 
+--             args = {'clock'} 
+--         },
+--         {
+--             label = "Change Clothes",
+--             event = "fobozo:print", 
+--             type = "client", 
+--             args = {'uniform'} 
+--         },
+--         {
+--             label = "Leave Conversation",
+--             event = "fobozo:print", 
+--             type = "client", 
+--             args = {} 
+--         }
+--     }
+-- )
